01e93abc76f6faa8821fe2e93363940fafbe03dc
唐思为 <111188489+tsw37616@users.noreply.github.com>
2024-12-15 13:41:45 +0800

    fix a bug

--- MyArchive.ApiService/Endpoints/ImageEndpoints.cs
+++ MyArchive.ApiService/Endpoints/ImageEndpoints.cs
@@ -12,7 +12,7 @@ namespace MyArchive.ApiService.Endpoints
             image.MapGet("/{imageName}", GetImageAsync);
         }
 
-        private static async Task<Results<Ok,NotFound>> GetImageAsync(string imageName, ImageService imageService, HttpContext context)
+        private static async Task GetImageAsync(string imageName, ImageService imageService, HttpContext context)
         {
             try
             {
@@ -21,22 +21,25 @@ namespace MyArchive.ApiService.Endpoints
                 {
                     context.Response.StatusCode = StatusCodes.Status404NotFound;
                     await context.Response.WriteAsync("Image not found");
-                    return TypedResults.NotFound();
+                  
                 }
 
                 var mimeType = imageService.GetMineType(imageName);
                 context.Response.ContentType = mimeType;
-                await context.Response.Body.WriteAsync(imageBytes);
-                return TypedResults.Ok();
+                context.Response.ContentLength = imageBytes.Length;
+
+                await context.Response.Body.WriteAsync(imageBytes, 0, imageBytes.Length);
+               
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
                 context.Response.StatusCode = StatusCodes.Status500InternalServerError;
                 await context.Response.WriteAsync("An error occurred while processing your request.");
-                return TypedResults.NotFound();
+                
             }
         }
+
     }
 
 
