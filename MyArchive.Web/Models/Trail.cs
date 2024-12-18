14554c292fbafa0e6b4d6d4471589f2e83eee301
唐思为 <111188489+tsw37616@users.noreply.github.com>
2024-12-18 21:01:11 +0800

    feat

---
+++ MyArchive.Web/Models/Trail.cs
@@ -0,0 +1,8 @@
+﻿namespace MyArchive.Web.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
--- MyArchive.Web/WebApiClient.cs
+++ MyArchive.Web/WebApiClient.cs
@@ -2,25 +2,7 @@ namespace MyArchive.Web;
 
 public class WebApiClient(HttpClient httpClient)
 {
-    public async Task<WeatherForecast[]> GetWeatherAsync(int maxItems = 10, CancellationToken cancellationToken = default)
-    {
-        List<WeatherForecast>? forecasts = null;
 
-        await foreach (var forecast in httpClient.GetFromJsonAsAsyncEnumerable<WeatherForecast>("/weatherforecast", cancellationToken))
-        {
-            if (forecasts?.Count >= maxItems)
-            {
-                break;
-            }
-            if (forecast is not null)
-            {
-                forecasts ??= [];
-                forecasts.Add(forecast);
-            }
-        }
-
-        return forecasts?.ToArray() ?? [];
-    }
 
     public async Task<string> GetImageDataAsync(string url)
     {
@@ -54,7 +36,3 @@ public class WebApiClient(HttpClient httpClient)
 
 }
 
-public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
