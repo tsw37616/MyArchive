2f4709d97f83736b1e7586d47b4baf8e30e486a3
唐思为 <111188489+tsw37616@users.noreply.github.com>
2025-02-09 22:12:52 +0800

    add database

---
+++ MyArchive.ApiService/Entitie/PostContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+namespace MyArchive.ApiService.Entitie
+{
+    public class PostContext: DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public string DbPath { get;  set; }
+
+        public PostContext()
+        {
+            DbPath = "E:/dev/repo/net9/MyArchive/db/post.db";
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+       => options.UseSqlite($"Data Source={DbPath}");
+
+    }
+}
